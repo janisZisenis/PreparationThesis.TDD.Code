807857f816c74cace0760211f520e2c39ddbecfd
Janis Zisenis <janis.zisenis@hotmail.de>
2017-09-27 10:34:36 +0200

    refactors GuiLayer to PreOrderIterator

--- src/CrossNative/GuiLayer/CNGuiLayer.cpp
+++ src/CrossNative/GuiLayer/CNGuiLayer.cpp
@@ -3,6 +3,8 @@
 #include "CNView.h"
 #include "CNMatcher.h"
 
+#include <stack>
+
 CNGuiLayerPtr CNGuiLayer::getNewInstance() {
     return CNGuiLayerPtr(new CNGuiLayer());
 }
@@ -42,12 +44,30 @@ CNViewPtr CNGuiLayer::findMatchingViewInHierarchy(CNMatcherPtr matcher, CNViewPt
 }
 
 CNViewPtr CNGuiLayer::findMatchingInChildren(CNMatcherPtr matcher, CNViewPtr parent) {
-    for (int i = 0; i < parent->getChildCount(); i++) {
-        CNViewPtr matchingView = findMatchingViewInHierarchy(matcher, parent->getChild(i));
+    std::stack<CNViewPtr> views;
 
-        if (matchingView)
-            return matchingView;
+    for (int i = 0; i < parent->getChildCount(); i++) {
+        views.push(parent->getChild(i));
     }
+
+    while(!views.empty()) {
+        if(isMatching(matcher, views.top()))
+            return views.top();
+        else {
+            CNViewPtr top = views.top();
+            views.pop();
+            for(int i = 0; i < top->getChildCount(); i++) {
+                views.push(top->getChild(i));
+            }
+        }
+    }
+
+//    for (int i = 0; i < parent->getChildCount(); i++) {
+//        CNViewPtr matchingView = findMatchingViewInHierarchy(matcher, parent->getChild(i));
+//
+//        if (matchingView)
+//            return matchingView;
+//    }
 
     return nullptr;
 }
@@ -55,3 +75,25 @@ CNViewPtr CNGuiLayer::findMatchingInChildren(CNMatcherPtr matcher, CNViewPtr par
 bool CNGuiLayer::isMatching(CNMatcherPtr matcher, CNViewPtr view) {
     return matcher->matches(view);
 }
+
+//class PreOrderIterator;
+//typedef std::shared_ptr<PreOrderIterator> PreOrderIteratorPtr;
+//class PreOrderIterator : public CNIterator {
+//public:
+//    static PreOrderIteratorPtr getNewInstance() {
+//        return PreOrderIteratorPtr(new PreOrderIterator());
+//    }
+//    virtual ~PreOrderIterator() {}
+//private:
+//    PreOrderIterator() {}
+//
+//public:
+//    virtual void first() override {}
+//    virtual void next() override {}
+//    virtual bool isDone() override {
+//        return false;
+//    }
+//    virtual CNViewPtr current() override {
+//        return nullptr;
+//    }
+//};
