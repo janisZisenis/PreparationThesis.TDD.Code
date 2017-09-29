9d6ae9ac55aabc2a08b0d5366f878ed2ee9beb2a
Janis Zisenis <janis.zisenis@hotmail.de>
2017-09-29 15:47:08 +0200

    refactors CNDynamicViewHierarchyTest: 	--> CNDynamicViewHierarchyTest, Loaded_View__LoadSubView_Matching__SubView_ShouldBeAddedTo_View 	--> CNDynamicViewHierarchyTest, Loaded_View_LoadSubView_NotMatching_ShouldNotBeAddedTo_View

--- src/CrossNative/CNDynamicViewHierarchy/CNDynamicViewHierarchyTest.cpp
+++ src/CrossNative/CNDynamicViewHierarchy/CNDynamicViewHierarchyTest.cpp
@@ -1,4 +1,6 @@
 #include <gmock/gmock.h>
+#include <string>
+
 #include "CNDynamicViewHierarchy.h"
 
 #include "CrossNative/CNView/CNViewTestDoubles.h"
@@ -8,30 +10,55 @@ protected:
     virtual CNDynamicViewHierarchyPtr makeCNDynamicViewHierarchy() {
         return CNDynamicViewHierarchy::getNewInstance();
     }
+    virtual CNViewSpyPtr makeCNViewSpy() {
+        return CNViewSpy::getNewInstance();
+    }
+
+    virtual CNViewDummyPtr makeCNViewDummy() {
+        return CNViewDummy::getNewInstance();
+    }
+
+    virtual void expectEquals(CNViewPtr actual, CNViewPtr expected, std::string errorMessage) {
+        EXPECT_THAT(actual, testing::Eq(expected)) << errorMessage;
+    }
+    virtual void expectUnequals(CNViewPtr actual, CNViewPtr unexpected, std::string errorMessage) {
+        EXPECT_THAT(actual, testing::Ne(unexpected)) << errorMessage;
+    }
+
+    virtual void expect_View_Added_View(CNViewSpyPtr receiver, CNViewPtr view, std::string errorMessage) {
+        CNViewPtr expected = view;
+        CNViewPtr actual = receiver->getAddedView();
+
+        expectEquals(actual, expected, errorMessage);
+    }
+    virtual void expect_Receiver_DidNotAdd_View(CNViewSpyPtr receiver, CNViewPtr view, std::string errorMessage) {
+        CNViewPtr unexpected = view;
+        CNViewPtr actual = receiver->getAddedView();
+
+        expectUnequals(actual, unexpected, errorMessage);
+    }
 };
 
 TEST_F(CNDynamicViewHierarchyTest, Loaded_View__LoadSubView_Matching__SubView_ShouldBeAddedTo_View) {
     CNDynamicViewHierarchyPtr sut = makeCNDynamicViewHierarchy();
-    CNViewSpyPtr view = CNViewSpy::getNewInstance();
+    CNViewSpyPtr view = makeCNViewSpy();
     sut->load(view, false);
 
-    CNViewDummyPtr subView = CNViewDummy::getNewInstance();
+    CNViewDummyPtr subView = makeCNViewDummy();
     sut->load(subView, true);
 
-    CNViewPtr actual = view->getAddedView();
-    CNViewPtr expected = subView;
-    EXPECT_THAT(actual, testing::Eq(expected)) << "SubView should be added to View, but it was not!";
+    std::string errorMessage = "SubView should be added to View, but it was not!";
+    expect_View_Added_View(view, subView, errorMessage);
 }
 
 TEST_F(CNDynamicViewHierarchyTest, Loaded_View_LoadSubView_NotMatching_ShouldNotBeAddedTo_View) {
     CNDynamicViewHierarchyPtr sut = makeCNDynamicViewHierarchy();
-    CNViewSpyPtr view = CNViewSpy::getNewInstance();
+    CNViewSpyPtr view = makeCNViewSpy();
     sut->load(view, false);
 
-    CNViewDummyPtr subView = CNViewDummy::getNewInstance();
+    CNViewDummyPtr subView = makeCNViewDummy();
     sut->load(subView, false);
 
-    CNViewPtr actual = view->getAddedView();
-    CNViewPtr expected = subView;
-    EXPECT_THAT(actual, testing::Ne(expected)) << "SubView should not be added to View, but it was!";
+    std::string errorMessage = "SubView should not be added to View, but it was!";
+    expect_Receiver_DidNotAdd_View(view, subView, errorMessage);
 }
