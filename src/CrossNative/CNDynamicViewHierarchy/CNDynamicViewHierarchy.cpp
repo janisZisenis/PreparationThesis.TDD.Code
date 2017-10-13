8e789a0f5866836364e7ad6d6048771c8e127370
Janis Zisenis <janis.zisenis@hotmail.de>
2017-10-13 11:06:52 +0200

    success: CNDynamicViewHierarchyTest, LoadedFirstView_LoadedSecondView__UnloadThirdView__ShouldThrowCNViewNotLoadedException

--- src/CrossNative/CNDynamicViewHierarchy/CNDynamicViewHierarchy.cpp
+++ src/CrossNative/CNDynamicViewHierarchy/CNDynamicViewHierarchy.cpp
@@ -25,6 +25,7 @@ void CNDynamicViewHierarchy::load(CNViewPtr view, CNMatcherPtr matcher) {
 void CNDynamicViewHierarchy::unload(CNViewPtr view) {
     if(views.size() == 0) throw CNViewNotLoadedException();
     if(views.size() == 1 && views[0] != view) throw CNViewNotLoadedException();
+    if(views.size() == 2 && views[1] != view) throw CNViewNotLoadedException();
 
     for(int i = 0; i < views.size(); i++)
         if(views[i]->isParentOf(view))
