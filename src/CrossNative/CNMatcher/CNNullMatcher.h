7b2fec16e632f8757047566a15b2271292d829e6
Janis Zisenis <janis.zisenis@hotmail.de>
2017-10-14 15:28:12 +0200

    adds CNNullMatcher

---
+++ src/CrossNative/CNMatcher/CNNullMatcher.h
@@ -0,0 +1,24 @@
+#ifndef CROSSNATIVE_NULLMATCHER_H
+#define CROSSNATIVE_NULLMATCHER_H
+
+#include "CNMatcher.h"
+
+class CNNullMatcher;
+typedef std::shared_ptr<CNNullMatcher> CNNullMatcherPtr;
+
+class CNNullMatcher : public CNMatcher {
+public:
+    static CNNullMatcherPtr getNewInstance() {
+        return CNNullMatcherPtr(new CNNullMatcher());
+    }
+    virtual ~CNNullMatcher() {};
+protected:
+    CNNullMatcher() {};
+
+public:
+    virtual bool matches(std::shared_ptr<CNComponent> component) {
+        return false;
+    }
+};
+
+#endif //CROSSNATIVE_NULLMATCHER_H
