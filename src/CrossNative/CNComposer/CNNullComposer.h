b1ebefdf31b7b5700a5b80ee17a2bf59015a67de
Janis Zisenis <janis.zisenis@hotmail.de>
2017-10-14 14:56:17 +0200

    adds CNNullComposer

---
+++ src/CrossNative/CNComposer/CNNullComposer.h
@@ -0,0 +1,23 @@
+#ifndef CROSSNATIVE_NULLCOMPOSER_H
+#define CROSSNATIVE_NULLCOMPOSER_H
+
+#include "CNComposer.h"
+
+class CNNullComposer;
+typedef std::shared_ptr<CNNullComposer> CNNullComposerPtr;
+class CNNullComposer : public CNComposer {
+public:
+    static CNNullComposerPtr getNewInstance() {
+        return CNNullComposerPtr(new CNNullComposer());
+    };
+    virtual ~CNNullComposer() {};
+
+protected:
+    CNNullComposer() {}
+
+public:
+    virtual void mount(std::shared_ptr<CNComponent> component) override {}
+    virtual void dismount(std::shared_ptr<CNComponent> component) override {}
+};
+
+#endif //CROSSNATIVE_NULLCOMPOSER_H
