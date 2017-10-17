462ec3631af8bf167e890e29dcbf1eb68a9f8b4f
Janis Zisenis <janis.zisenis@hotmail.de>
2017-10-17 11:52:29 +0200

    adds (fails): CNTagIdentifyingVisitorTest, FreshInstance__VisitTaggedWithDifferentTag__ShouldNotIdentifyTagged

--- src/CrossNative/CNMatcher/Visitors/CNTagIdentifyingVisitorTest.cpp
+++ src/CrossNative/CNMatcher/Visitors/CNTagIdentifyingVisitorTest.cpp
@@ -17,9 +17,15 @@ protected:
         std::string errorMessage = "CNTagIdentifyingVisitor should have identified the CNTagged, but it has not!";
         EXPECT_TRUE(actual) << errorMessage;
     }
+    virtual void expectHasNotIdentified(CNTagIdentifyingVisitorPtr sut) {
+        bool actual = sut->hasIdentified();
+
+        std::string errorMessage = "CNTagIdentifyingVisitor should not have identified the CNTagged, but it has!";
+        EXPECT_FALSE(actual) << errorMessage;
+    }
 };
 
-TEST_F(CNTagIdentifyingVisitorTest, FreshInstance__VisitTaggedWithTheSameTag__ShouldIdentifyTagged) {
+TEST_F(CNTagIdentifyingVisitorTest, FreshInstance__VisitTaggedWithSameTag__ShouldIdentifyTagged) {
     CNTagIdentifyingVisitorPtr sut = makeCNTagIdentifyingVisitor("theTag");
 
     CNTaggedStubPtr tagged = makeCNTaggedStub();
@@ -29,3 +35,12 @@ TEST_F(CNTagIdentifyingVisitorTest, FreshInstance__VisitTaggedWithTheSameTag__Sh
     expectHasIdentified(sut);
 }
 
+TEST_F(CNTagIdentifyingVisitorTest, FreshInstance__VisitTaggedWithDifferentTag__ShouldNotIdentifyTagged) {
+    CNTagIdentifyingVisitorPtr sut = makeCNTagIdentifyingVisitor("theTag");
+
+    CNTaggedStubPtr tagged = makeCNTaggedStub();
+    tagged->setTag("differentTag");
+    sut->visit(tagged);
+
+    expectHasNotIdentified(sut);
+}
