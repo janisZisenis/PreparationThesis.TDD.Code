3f67bd369e0cd842821666f553025daed03cf02a
Janis Zisenis <janis.zisenis@hotmail.de>
2017-10-18 16:19:44 +0200

    links QtView against CodeBase

--- applications/QtViews/3rdPartyLinking.cmake
+++ applications/QtViews/3rdPartyLinking.cmake
@@ -1,2 +1,5 @@
 include(${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/Qt/Link.cmake)
 linkQt(${targetName})
+
+target_include_directories(${targetName} PUBLIC "/Users/Janis/Development/CodeBase/src")
+target_link_libraries(${targetName} /Users/Janis/Development/CodeBase/build/applications/JZCoreImp/libJZCoreImp.dylib)
