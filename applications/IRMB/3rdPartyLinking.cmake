bc19784587582725c89563a3387f054f782ace98
Janis Zisenis <janis.zisenis@hotmail.de>
2017-10-21 19:08:35 +0200

    adds AddGridGeneratorAction

--- applications/IRMB/3rdPartyLinking.cmake
+++ applications/IRMB/3rdPartyLinking.cmake
@@ -1,2 +1,5 @@
 include(${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/Boost/Link.cmake)
 linkBoost(${targetName})
+
+target_include_directories(${targetName} PUBLIC "/Users/Janis/Development/CodeBase/src")
+target_link_libraries(${targetName} /Users/Janis/Development/CodeBase/build/applications/CodeBaseImp/libCodeBaseImp.dylib)
