5605bf52ff22d1aa0aef3952f948f8e9dc63f9a8
Janis Zisenis <janis.zisenis@hotmail.de>
2017-10-23 10:24:59 +0200

    adds target QtIRMB.main

---
+++ applications/QtIRMB.main/QtFileFinder.h
@@ -0,0 +1,25 @@
+#ifndef QTIRMB_QTFILEFINDER_H
+#define QTIRMB_QTFILEFINDER_H
+
+#include "IRMB/TransActions/AddSTLFileAction/FileFinder.h"
+
+
+class QtFileFinder;
+typedef std::shared_ptr<QtFileFinder> QtFileFinderPtr;
+
+class QtFileFinder : public FileFinder {
+public:
+    static QtFileFinderPtr getNewInstance();
+    virtual ~QtFileFinder();
+private:
+    QtFileFinder();
+
+public:
+    virtual bool findFile() override;
+    virtual std::string getFileName() override;
+
+private:
+    std::string foundFile;
+};
+
+#endif //QTIRMB_QTFILEFINDER_H
