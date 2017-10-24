670249c678faf274f630f6db4db07903baa442c6
Janis Zisenis <janis.zisenis@hotmail.de>
2017-10-24 13:50:09 +0200

    extracts GridGenerator and VTKWriter in 3rdParty libs

--- applications/GridGenerator/3rdPartyLinking.cmake
+++
@@ -1,5 +0,0 @@
-include(${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/Metis/Link.cmake)
-linkMetis(${targetName})
-
-include(${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/MPI/Link.cmake)
-linkMPI(${targetName})
--- applications/IRMB/3rdPartyLinking.cmake
+++ applications/IRMB/3rdPartyLinking.cmake
@@ -3,3 +3,9 @@ linkBoost(${targetName} COMPONENTS filesystem)
 
 target_include_directories(${targetName} PUBLIC "/Users/Janis/Development/CodeBase/src")
 target_link_libraries(${targetName} /Users/Janis/Development/CodeBase/build/applications/CodeBaseImp/libCodeBaseImp.dylib)
+
+target_include_directories(${targetName} PUBLIC "/Users/Janis/Development/GridGenerator/src")
+target_link_libraries(${targetName} /Users/Janis/Development/GridGenerator/build/applications/GridGenerator/libGridGenerator.dylib)
+
+target_include_directories(${targetName} PRIVATE "/Users/Janis/Development/GridGenerator/src")
+target_link_libraries(${targetName} /Users/Janis/Development/GridGenerator/build/applications/VTKWriter/libVTKWriter.dylib)
