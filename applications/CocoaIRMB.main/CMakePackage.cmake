8f0311170cabaab24e24feb200ad6b7810136803
Janis Zisenis <janis.zisenis@hotmail.de>
2017-10-24 14:18:13 +0200

    removes all targets but "AllTest CrossNative/CrossNativeTest CrossViews/CrossViewsTest QtViews CocoaViews"

--- applications/CocoaIRMB.main/CMakePackage.cmake
+++
@@ -1,7 +0,0 @@
-#FILE ENDINGS
-resetFileEndingsToCollect()
-addObjCAndObjCPPFileTypesToCollect()
-
-#GLOB SOURCE FILES IN MY_SRCS
-unset(MY_SRCS)
-includeRecursiveAllFilesFrom(${targetName} ${CMAKE_CURRENT_LIST_DIR})
